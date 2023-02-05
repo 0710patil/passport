79e89f4030b3436a206db40dd1e301a2096eb1b8
shivdiksha patil <shivdiksha.patil@neutrinos.co>
2023-02-05 16:37:07 +0530

    code

---
+++ passportcon.js
@@ -0,0 +1,13 @@
+const LocalStrategy = require('passport-local').Strategy;
+const {User} = require('./database');
+exports.startPassport = (passport) => {
+    passport.use(new LocalStrategy(async(username,password,success)=>{
+    const user = await User.findOne({username});
+    
+    if(!user) return success (null,false);
+    if(user.password !== password) return success (null,false);
+    return success(null,user);
+}
+
+    ))
+
