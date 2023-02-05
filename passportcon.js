9184a3cb8d440c309145d7c7249a7c11234dce54
shivdiksha patil <shivdiksha.patil@neutrinos.co>
2023-02-05 17:30:42 +0530

    update

--- passportcon.js
+++ passportcon.js
@@ -3,10 +3,14 @@ const { User } = require("./database");
 exports.startPassport = (passport) => {
   passport.use(
     new LocalStrategy(async (username, password, success) => {
-      const user = await User.findOne({ username });
+      try {
+        const user = await User.findOne({ username });
+
+        if (!user) return success(null, false);
+        if (user.password !== password) return success(null, user);
+      } catch (err) {
+        return success(err, false);
+      }
     })
   );
-  if (!user) return success(null, false);
-  if (user.password !== password) return success(null, false);
-  return success(null, user);
 };
